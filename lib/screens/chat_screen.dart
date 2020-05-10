2fb086722e7b997c2ed796c8165e717935a92ab0
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-10 13:20:31 -0500

    Listening to Chat Messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widgets/chat/messages.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -29,34 +30,21 @@ class ChatScreen extends StatelessWidget {
               ),
             ],
             onChanged: (itemIdentifier) {
-              if(itemIdentifier == 'logout') {
+              if (itemIdentifier == 'logout') {
                 FirebaseAuth.instance.signOut();
               }
             },
           )
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection('chats/ZwKnAoYNaTJVNlkvwH5t/messages')
-            .snapshots(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          final documents = snapshot.data.documents;
-
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(documents[index]['text']),
-              );
-            },
-          );
-        },
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Messages(),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
