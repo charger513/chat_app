e4f483ce8f3e76872c4b479f25e26281e0dcf7db
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-11 18:02:15 -0500

    Improving the Chat Bubbles

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,24 +1,43 @@
 import 'package:chat_app/widgets/chat/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance.collection('chat').orderBy('createdAt', descending: true).snapshots(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder:
+          (BuildContext context, AsyncSnapshot<FirebaseUser> futureSnapshot) {
+        if (futureSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
+        return StreamBuilder(
+          stream: Firestore.instance
+              .collection('chat')
+              .orderBy('createdAt', descending: true)
+              .snapshots(),
+          builder: (BuildContext context, AsyncSnapshot chatSnapshot) {
+            if (chatSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
 
-        final chatDocs = snapshot.data.documents;
+            final chatDocs = chatSnapshot.data.documents;
 
-        return ListView.builder(
-          reverse: true,
-          itemCount: chatDocs.length,
-          itemBuilder: (BuildContext context, int index) {
-            return MessageBubble(chatDocs[index]['text']);
+            return ListView.builder(
+              reverse: true,
+              itemCount: chatDocs.length,
+              itemBuilder: (BuildContext context, int index) {
+                return MessageBubble(
+                  chatDocs[index]['text'],
+                  chatDocs[index]['userId'] == futureSnapshot.data.uid,
+                  key: ValueKey(chatDocs[index].documentID),
+                );
+              },
+            );
           },
         );
       },
