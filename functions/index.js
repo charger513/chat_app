9385cfa7e46ef5de6dcb6280e2e1cdd5729656a1
Emmanuel Chargoy <charger513@hotmail.com>
2020-05-17 10:56:37 -0500

    Setting a Firestore Trigger

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,10 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.myFunction = functions.firestore
+  .document('chat/{message}')
+  .onCreate((snap, context) => {
+    console.log(snap.data());
+    return;
+  });
